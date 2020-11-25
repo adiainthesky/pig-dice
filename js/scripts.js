58b838d75089cc617a482604e3d0d1e495a4444f
Adelaide Nalley <adiainthesky@gmail.com>
2020-11-25 12:49:53 -0800

    add two player turn-taking

--- js/scripts.js
+++ js/scripts.js
@@ -13,13 +13,81 @@ $(document).ready(function(event) {
     playerOne = new Player(prompt("Player one, please enter your name:"));
     playerTwo = new Player(prompt("Player two, please enter your name:"));
     console.log(playerOne.name);
-    console.log(playerTwo.name);
-    
+    console.log(playerTwo.name);   
     currentPlayer = playerOne;
     currentPlayer.turn();
-
 });
     
+Player.prototype.turn = function() {
+  alert("Choose to roll or to hold")
+  $(".hold").show();
+  $(".roll").show();
+  $(".roll").click(function(){
+    currentPlayer.roll();
+  }); 
+  $(".hold").click(function(){
+    currentPlayer.hold();
+    currentPlayer.choice();
+  }); 
+}
+
+Player.prototype.roll = function() {
+  let dice = Math.floor((Math.random() * 6) + 1);  
+  console.log("dice =" + dice)
+  currentPlayer.addToTurnScore(dice);
+  currentPlayer.choice();
+}
+
+Player.prototype.choice = function (){
+  $(".hold").show();
+  $(".roll").show();
+}
+
+Player.prototype.addToTurnScore = function(amount) {
+  this.turnScore += amount;  
+  console.log("turnScore =" + this.turnScore)
+}
+
+Player.prototype.hold = function() {
+  this.addToTotalScore(this.turnScore);  
+  currentPlayer.switchPlayer();
+  console.log("currentPlayer =" + currentPlayer.name);
+  $(".hold").hide();
+  $(".roll").hide();
+}
+
+Player.prototype.addToTotalScore = function(amount) {
+  this.totalScore += amount;
+  this.turnScore = 0;
+  console.log("Player1 totalScore =" + playerOne.totalScore)
+  console.log("Player2 totalScore =" + playerTwo.totalScore)
+}
+
+Player.prototype.switchPlayer = function() {
+  if(currentPlayer === playerOne) {
+      currentPlayer = playerTwo;
+  }
+  else {
+    currentPlayer = playerOne;
+    alert(currentPlayer.name + ", its your turn now");
+  }
+  console.log("2currentPlayer =" + currentPlayer.name)
+  currentPlayer.choice();
+}
+
+function game(playerOne, playerTwo) {
+  while (playerOne.score < 100 && playerTwo.score < 100) {
+    playerOne.turn(); 
+    console.log(playerOne.score);
+    playerTwo.turn();
+  }
+  if (playerOne.score >= 100) {
+    console.log("Congrats " + playerOne.name + "!")
+  }
+  else {
+    console.log("Congrats " + playerTwo.name + "!");
+  }
+}
 
 
 /* $(".roll").click(function(){
@@ -34,85 +102,14 @@ $(document).ready(function(event) {
  */
 // BUSINESS
 
-Player.prototype.choice = function (){
-  $(".hold").show();
-  $(".roll").show();
-}
 
-Player.prototype.roll = function() {
-  let dice = Math.floor((Math.random() * 6) + 1);  
-  console.log("dice =" + dice)
-  currentPlayer.addToTurnScore(dice);
-  currentPlayer.choice();
-}
 
-Player.prototype.addToTurnScore = function(amount) {
-  this.turnScore += amount;  
-  console.log("turnScore =" + this.turnScore)
-}
 
-Player.prototype.hold = function() {
-  this.addToTotalScore(this.turnScore);  
-  currentPlayer.switchPlayer();
-  console.log("currentPlayer =" + currentPlayer.name);
-
-  $(".hold").hide();
-  $(".roll").hide();
-}
-
-Player.prototype.addToTotalScore = function(amount) {
-  this.totalScore += amount;
-  this.turnScore = 0;
-  console.log("totalScore =" + this.totalScore)
-  console.log("2turnScore =" + this.turnScore)
-}
-
-Player.prototype.turn = function() {
-  alert("Choose to roll or to hold")
-  $(".hold").show();
-  $(".roll").show();
-  $(".roll").click(function(){
-    currentPlayer.roll();
-  }); 
-
-  $(".hold").click(function(){
-    currentPlayer.hold();
-  }); 
-
-  
-}
-
-Player.prototype.switchPlayer = function() {
-  if(currentPlayer === playerOne) {
-      currentPlayer = playerTwo;
-  }
-  else {
-    currentPlayer = playerOne;
-    alert(currentPlayer.name + ", its your turn now");
-  }
-  console.log("2currentPlayer =" + currentPlayer.name)
-  return currentPlayer;
-  
-}
 
     
 
 
 
-    // function game(playerOne, playerTwo) {
-      
-    //   while (playerOne.score < 100 && playerTwo.score < 100) {
-    //     playerOne.turn(); 
-    //     console.log(playerOne.score);
-    //     playerTwo.turn();
-    //   }
-    //   if (playerOne.score >= 100) {
-    //     console.log("Congrats " + playerOne.name + "!")
-    //   }
-    //   else {
-    //     console.log("Congrats " + playerTwo.name + "!");
-    //   }
-    // }
 
 
 
@@ -220,14 +217,6 @@ Player.prototype.switchPlayer = function() {
 //   // console.log(playerOne.score)
 //   // console.log(playerOne.name);  
 // };
-
-
-//   // ask: hold or roll ...  (sum of rolls, if one = 0)
-//   // this.score += turnScore
-
-// Player.prototype.roll = function() {
-//  return Math.floor((Math.random() * 6) + 1);
-// }
 
 
 // // user interface:
